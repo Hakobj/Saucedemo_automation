0e71324c7c8da5384975bca9b551856b44271eb8
Hakob Julfayan <julfayanhakob91@gmail.com>
2025-07-02 17:19:43 +0400

    commenting password manager part to solve CI problem

--- conftest.py
+++ conftest.py
@@ -7,9 +7,9 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     # Setup
     chrome_options = Options()
-    chrome_options.add_experimental_option("prefs", {
-        "profile.password_manager_leak_detection": False
-    })
+    # chrome_options.add_experimental_option("prefs", {
+    #     "profile.password_manager_leak_detection": False
+    # })
 
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
